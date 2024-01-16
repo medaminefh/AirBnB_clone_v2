3a781b7516f4decf835a04ea747cfa1342424431
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-01-16 19:08:46 +0100

    update reviews

--- models/place.py
+++ models/place.py
@@ -2,10 +2,11 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage_type
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     if storage_type == "db":
         __tablename__ = 'places'
@@ -19,6 +20,7 @@ class Place(BaseModel):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=False)
         longitude = Column(Float, nullable=False)
+        reviews = relationship("Review", backref="place", cascade="all, delete")
 
     else:
         city_id = ""
@@ -31,3 +33,14 @@ class Place(BaseModel):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
+
+        @property
+        def reviews(self):
+            """ Getter attribute in case of file storage """
+            from models import storage
+            from models.review import Review
+            review_list = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
